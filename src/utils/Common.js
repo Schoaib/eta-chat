90c574ca97b9ad097d57b4a4a0ff5e36a279581d
Syed Shoaib <muhammadshoaib.syed@dnata.com>
2017-11-13 22:29:28 +0400

    init

---
+++ src/utils/Common.js
@@ -0,0 +1,21 @@
+// @flow
+module.exports = function() {
+    const crypto = require('crypto'),
+        algorithm = 'aes-256-ctr',
+        password = 'v!aF0n3$';
+    var commonFunctions = {
+        encrypt: function(text) {
+            var cipher = crypto.createCipher(algorithm, password)
+            var crypted = cipher.update(text, 'utf8', 'hex')
+            crypted += cipher.final('hex');
+            return crypted;
+        },
+        decrypt: function(text) {
+            var decipher = crypto.createDecipher(algorithm, password)
+            var dec = decipher.update(text, 'hex', 'utf8')
+            dec += decipher.final('utf8');
+            return dec;
+        }
+    };
+    return commonFunctions;
+}();
