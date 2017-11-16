473829fc0bbae4d4aec93cb3daf8c09a7f8d68a8
Syed Shoaib <muhammadshoaib.syed@dnata.com>
2017-11-16 20:35:27 +0400

    dev

--- src/app.js
+++ src/app.js
@@ -642,31 +642,70 @@ function processEvent(event) {
 
               var messageData = {
                 "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "airline_update",
-            "intro_message": "Your flight is delayed",
-            "locale": "en_US",
-            "update_flight_info": {
-              "flight_number": "KL123",
-              "departure_airport": {
-                "airport_code": "SFO",
-                "city": "San Francisco",
-                "terminal": "T4",
-              },
-              "arrival_airport": {
-                "airport_code": "AMS",
-                "city": "Amsterdam",
-                "terminal": "T4",
-              },
-              "flight_schedule": {
-                "departure_time": "2015-12-26T11:30",
-                "arrival_time": "2015-12-27T07:30"
+        "type": "template",
+        "payload": {
+          "template_type": "airline_boardingpass",
+          "intro_message": "You are checked in.",
+          "locale": "en_US",
+          "boarding_pass": [
+            {
+              "passenger_name": "SMITH\/NICOLAS",
+              "pnr_number": "CG4X7U",
+              "seat": "74J",
+              "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
+              "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+              "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+              "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+              "auxiliary_fields": [
+                {
+                  "label": "Terminal",
+                  "value": "T1"
+                },
+                {
+                  "label": "Departure",
+                  "value": "30OCT 19:05"
+                }
+              ],
+              "secondary_fields": [
+                {
+                  "label": "Boarding",
+                  "value": "18:30"
+                },
+                {
+                  "label": "Gate",
+                  "value": "D57"
+                },
+                {
+                  "label": "Seat",
+                  "value": "74J"
+                },
+                {
+                  "label": "Sec.Nr.",
+                  "value": "003"
+                }
+              ],
+              "flight_info": {
+                "flight_number": "KL0642",
+                "departure_airport": {
+                  "airport_code": "JFK",
+                  "city": "New York",
+                  "terminal": "T1",
+                  "gate": "D57"
+                },
+                "arrival_airport": {
+                  "airport_code": "AMS",
+                  "city": "Amsterdam"
+                },
+                "flight_schedule": {
+                  "departure_time": "2016-01-02T19:05",
+                  "arrival_time": "2016-01-05T17:30"
+                }
               }
             }
-          }
+          ]
         }
-              };
+      }
+    }
               sendFBMessage(sender,messageData);
 
               break;
