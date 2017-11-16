1197e3b4374e004a4d54fad94a6602cc4e16adb1
Syed Shoaib <muhammadshoaib.syed@dnata.com>
2017-11-16 20:21:59 +0400

    dev

--- src/app.js
+++ src/app.js
@@ -639,6 +639,41 @@ function processEvent(event) {
             case 'flight.search':
               console.log('inside flight')
               console.log('flight.search',response.result.parameters)
+
+              var messageData = {
+                "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "airline_update",
+            "intro_message": "Your flight is delayed",
+            "update_type": "delay",
+            "locale": "en_US",
+            "pnr_number": "CF23G2",
+            "update_flight_info": {
+              "flight_number": "KL123",
+              "departure_airport": {
+                "airport_code": "SFO",
+                "city": "San Francisco",
+                "terminal": "T4",
+                "gate": "G8"
+              },
+              "arrival_airport": {
+                "airport_code": "AMS",
+                "city": "Amsterdam",
+                "terminal": "T4",
+                "gate": "G8"
+              },
+              "flight_schedule": {
+                "boarding_time": "2015-12-26T10:30",
+                "departure_time": "2015-12-26T11:30",
+                "arrival_time": "2015-12-27T07:30"
+              }
+            }
+          }
+        }
+              };
+              sendFBMessage(sender,message);
+
               break;
             case 'Search':
               console.log('sender123', sender)
