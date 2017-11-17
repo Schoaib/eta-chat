2f28b8fd02a69df56ac19b283439aeca99584986
Syed Shoaib <muhammadshoaib.syed@dnata.com>
2017-11-18 01:31:55 +0400

    dev

--- src/app.js
+++ src/app.js
@@ -391,12 +391,12 @@ class FacebookBot {
               console.log('parsedJSON',parsedJSON)
               var testJson = [
                 {type:1 ,
-                  imageUrl: "https://s3-ap-southeast-1.amazonaws.com/tempviafone/flight1_1.PNG",
+                  imageUrl: "https://s3-ap-southeast-1.amazonaws.com/tempviafone/flight12.PNG",
                    title: "title1",
                    subtitle: "subtitle1",
                  buttons:[]},
                    {type:1 ,
-                     imageUrl: "https://s3-ap-southeast-1.amazonaws.com/tempviafone/flight2.PNG",
+                     imageUrl: "https://s3-ap-southeast-1.amazonaws.com/tempviafone/flight12.PNG",
                       title: "title2", subtitle: "subtitle2",buttons:[]}];
               responseMessages=testJson;
               this.doRichContentResponse(sender, responseMessages);
