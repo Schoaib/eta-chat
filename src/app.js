3cd07c9b8db89d4bf6d06e39423ee797e35083da
Syed Shoaib <muhammadshoaib.syed@dnata.com>
2017-12-05 13:04:47 +0400

    dev

--- src/app.js
+++ src/app.js
@@ -542,7 +542,8 @@ class FacebookBot {
             console.log('args.data', args.data)
             client.post(WEATHER_API + 'translateLan', args, function(data, response) {
               console.log('data', data)
-              if (data && data.translations && data.translations.length >0 ) {
+              if (data && data.translations) {
+                console.log('data.translations', data.translations)
                 that.doTextResponse(sender, data.translations[0].translatedText);
               } else {
                 that.doTextResponse(sender, "Can you please be more specific?");
